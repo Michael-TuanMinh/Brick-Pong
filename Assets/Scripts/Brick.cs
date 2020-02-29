953f7c2527c121c723d16e9346bdf16755c463f0
Michael Vu <vuminh99hp@gmail.com>
2020-02-29 15:32:39 -0500

    Input Mouse Drag

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -9,50 +9,21 @@ public class Brick : MonoBehaviour
 
     private Rigidbody2D rb;
     private float deltaX, deltaY;
-   
-
+    private bool isDraging;
+    private Vector2 touchPos;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-
+    
     private void Update()
     {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-
-            switch (touch.phase)
-            {
-                case TouchPhase.Began:
-                    deltaX = touchPos.x - transform.position.x;
-                    deltaY = touchPos.y - transform.position.y;
-                    break;
-                case TouchPhase.Moved:
-
-                    if(transform.parent.GetComponent<PlayerController>().isAtLeftBorder && touchPos.x - deltaX > transform.position.x) // move to the left
-                    {
-                        rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
-                    }
-                    else if (transform.parent.GetComponent<PlayerController>().isAtRightBorder && touchPos.x - deltaX < transform.position.x) // move to the right
-                    {
-                        rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
-                    }
-
-                    else if (!transform.parent.GetComponent<PlayerController>().isAtLeftBorder && !transform.parent.GetComponent<PlayerController>().isAtRightBorder)
-                    {
-                        rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
-                    }
-                    break;
-
-                case TouchPhase.Ended:
-                    rb.velocity = Vector2.zero;
-                    break;
-            }
-        }
+#if UNITY_EDITOR
+        InputMouseDrag();
+#else
+        InputTouchDrag();
+#endif
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -101,6 +72,76 @@ public class Brick : MonoBehaviour
         }
     }
 
+    private void InputMouseDrag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDraging = true;
+            touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            deltaX = touchPos.x - transform.position.x;
+            deltaY = touchPos.y - transform.position.y;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDraging = false;
+        }
 
+        if (isDraging)
+        {
+            touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (transform.parent.GetComponent<PlayerController>().isAtLeftBorder && touchPos.x - deltaX > transform.position.x) // move to the left
+            {
+                rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+            }
+            else if (transform.parent.GetComponent<PlayerController>().isAtRightBorder && touchPos.x - deltaX < transform.position.x) // move to the right
+            {
+                rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+            }
+
+            else if (!transform.parent.GetComponent<PlayerController>().isAtLeftBorder && !transform.parent.GetComponent<PlayerController>().isAtRightBorder)
+            {
+                rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+            }
+
+        }
+    }
+
+    private void InputTouchDrag()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    deltaX = touchPos.x - transform.position.x;
+                    deltaY = touchPos.y - transform.position.y;
+                    break;
+                case TouchPhase.Moved:
+
+                    if (transform.parent.GetComponent<PlayerController>().isAtLeftBorder && touchPos.x - deltaX > transform.position.x) // move to the left
+                    {
+                        rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+                    }
+                    else if (transform.parent.GetComponent<PlayerController>().isAtRightBorder && touchPos.x - deltaX < transform.position.x) // move to the right
+                    {
+                        rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+                    }
+
+                    else if (!transform.parent.GetComponent<PlayerController>().isAtLeftBorder && !transform.parent.GetComponent<PlayerController>().isAtRightBorder)
+                    {
+                        rb.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    rb.velocity = Vector2.zero;
+                    break;
+            }
+        }
+    }
 
 }
